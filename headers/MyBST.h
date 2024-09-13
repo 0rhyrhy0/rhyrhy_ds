87221c8c13db4f6b11a4b57b9174f2943f6790ed
0rhyrhy0 <rhy123123@naver.com>
2024-09-14 03:43:56 +0900

    MyBST] Add docstrings and functions - Added : Copy constructor, Destructor - Added : traverse functions (three of them) - Added : erase - Modified : remove

--- headers/MyBST.h
+++ headers/MyBST.h
@@ -7,15 +7,69 @@
  */
 class MyBST {
 public:
+    /**
+     * @brief Construct new MyBST root node with its value as `root_value`.
+     * @param root_value The value root will have.
+     */
     MyBST(int root_value = 0);
 
+    /**
+     * @brief Copy constructor.
+     * @param other The MyBST to copy from.
+     */
+    MyBST(const MyBST& other);
+
+    // No move constructor yet
+
+    /**
+     * @brief Destructor.
+     * Recursively deletes all child nodes, effectively deleting the entire subtree.
+     */
+    ~MyBST();
+
+    /**
+     * @brief Insert value.
+     * Do nothing if the value already exists.
+     * 
+     * @param value The value being inserted.
+     */
     MyBST* insert(int value);
+
+    /**
+     * @brief Remove value.
+     * @param value The value being removed.
+     * 
+     * @throws `logic_error` if the value was not found.
+     */
     MyBST* remove(int value);
 
+    /**
+     * @brief Find the node with value.
+     * @param value The value being found.
+     * 
+     * @returns 
+     * - `MyBST*` if the value was found.
+     * 
+     * - `nullptr` if the value was not found.
+     */
     MyBST* find(int value);
 
-    MyBST* smallest() { return left  ? left->smallest() : this; }
-    MyBST* biggest()  { return right ? right->biggest() : this; }
+    // Traverse the tree in order.
+    void in_order_traverse() const;
+
+    // Traverse the tree in post-order.
+    void post_order_traverse() const;
+
+    // Traverse the tree in pre-order.
+    void pre_order_traverse() const;
+
+    // Find node with smallest value.
+    MyBST* smallest() { return left ? left->smallest() : this; }
+
+    // Find node with biggest value.
+    MyBST* biggest() { return right ? right->biggest() : this; }
+
+    // Const reference to the `_value`.
     const int& value() const { return _value; }
 
 private:
@@ -23,6 +77,16 @@ private:
     MyBST* parent;
     MyBST* left;
     MyBST* right;
+
+    /**
+     * @brief Erase a single node, not the whole subtree.
+     * @param node The node you're erasing.
+     * 
+     * Use this in case you have to remove only a single node.
+     * 
+     * Before using it, make sure the node is not part of the tree anymore.
+     */
+    void erase(MyBST* node);
 };
 
 #endif
--- src/MyBST.cpp
+++ src/MyBST.cpp
@@ -1,8 +1,31 @@
 #include "../headers/MyBST.h"
-#include <iostream> // logic_error
+#include <iostream> // logic_error, cout
 
 MyBST::MyBST(int root_value)
     : _value(root_value), parent(nullptr), left(nullptr), right(nullptr) {}
+
+MyBST::MyBST(const MyBST& other) 
+    : _value(other._value), parent(nullptr), left(nullptr), right(nullptr) {
+
+    if(other.left)
+        ( left = new MyBST(*other.left) ) -> parent = this;
+
+    if(other.right)
+        ( right = new MyBST(*other.right) ) -> parent = this;
+}
+
+MyBST::~MyBST() {
+    delete left;
+    delete right;
+
+    // disconnect from parent node if it exists
+    if (parent) {
+        if (this == parent->left)
+            parent->left = nullptr;
+        else if (this == parent->right)
+            parent->right = nullptr;
+    }
+}
 
 MyBST* MyBST::insert(int value) {
     if (value < _value) {
@@ -44,9 +67,13 @@ MyBST* MyBST::remove(int value) {
             removing_node->parent->right = child;
 
         child->parent = removing_node->parent;
-        delete removing_node;
 
-        if(removing_node  == this) return child; // `child` is now root
+        if(removing_node == this) {
+            erase(removing_node);
+            return child; // `child` is now root
+        }
+
+        erase(removing_node);
         return this;
     }
 
@@ -57,7 +84,7 @@ MyBST* MyBST::remove(int value) {
         else
             removing_node->parent->right = nullptr;
     }
-    delete removing_node;
+    erase(removing_node);
 
     if(removing_node == this) return nullptr; // the only node is removed
     return this;
@@ -68,4 +95,29 @@ MyBST* MyBST::find(int value) {
 
     if (value > _value) return right ? right->find(value) : nullptr;
     else                return left  ? left ->find(value) : nullptr;
+}
+
+void MyBST::in_order_traverse() const {
+    if(left) left->in_order_traverse();
+    std::cout << _value << ' ';
+    if(right) right->in_order_traverse();
+}
+
+void MyBST::post_order_traverse() const {
+    if(left) left->post_order_traverse();
+    if(right) right->post_order_traverse();
+    std::cout << _value << ' ';
+}
+
+void MyBST::pre_order_traverse() const {
+    std::cout << _value << ' ';
+    if(left) left->pre_order_traverse();
+    if(right) right->pre_order_traverse();
+}
+
+void MyBST::erase(MyBST* node) {
+    node->parent = nullptr;
+    node->left = nullptr;
+    node->right = nullptr;
+    delete node;
 }
